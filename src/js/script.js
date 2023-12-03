ab5c244dddc264a5fcdbd30b3a4554bbfac3ca3d
j6a6pedr6 <jbezerradossantos1@gmail.com>
2023-12-03 14:22:09 -0300

    segundo commit

--- src/js/script.js
+++ src/js/script.js
@@ -11,4 +11,14 @@ let heroi = new hero ("Jorge", 33, "guerreiro")
 
 let attack = "espada"
 
+if (tipo = "guerreiro") {
+    attack = "espada"
+} else if (tipo = "monge") {
+    attack = "artes marciais"
+} else if (tipo = "mago") {
+    attack = "magia"
+} else if (tipo = "ninja") {
+    attack = "shiriken"
+}
+
 console.log("o " + heroi.tipo + " atacou usando " + attack)
